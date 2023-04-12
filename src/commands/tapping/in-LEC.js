8c9d207ca4f64ef5a929e27238400d30cceddec0
Immov <41504515+Immov@users.noreply.github.com>
2023-04-12 11:19:27 +0700

    fix time

--- src/commands/tapping/in-LEC.js
+++ src/commands/tapping/in-LEC.js
@@ -2,15 +2,6 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const tap = require('../../models/tap');
 const { insertDB } = require('../../functions/insertDB');
 
-const now = new Date();
-let hour = now.getHours();
-let minute = now.getMinutes();
-hour = hour <= 9 ? '0' + hour : hour;
-minute = minute <= 9 ? '0' + minute : minute;
-const zone = (now.getTimezoneOffset() / 60) * -1;
-const unixTime = (Date.now() / 1000) | 0;
-const timestamp = `<t:${unixTime}:t> - <t:${unixTime}:d>`;
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('inlec')
@@ -22,6 +13,15 @@ module.exports = {
 				.setRequired(true)
 		),
 	async execute(interaction) {
+		const now = new Date();
+		let hour = now.getHours();
+		let minute = now.getMinutes();
+		hour = hour <= 9 ? '0' + hour : hour;
+		minute = minute <= 9 ? '0' + minute : minute;
+		const zone = (now.getTimezoneOffset() / 60) * -1;
+		const unixTime = (Date.now() / 1000) | 0;
+		const timestamp = `<t:${unixTime}:t> - <t:${unixTime}:d>`;
+
 		const img = interaction.options.getAttachment('proof').url;
 		const message = `[SIGN IN][LEC]: ${interaction.user.tag} - ${hour}:${minute} [GMT]: ${zone} [PROOF]: ${img}`;
 		const output = new EmbedBuilder()
--- src/commands/tapping/out.js
+++ src/commands/tapping/out.js
@@ -2,20 +2,20 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const tap = require('../../models/tap');
 const { insertDB } = require('../../functions/insertDB');
 
-const now = new Date();
-let hour = now.getHours();
-let minute = now.getMinutes();
-hour = hour <= 9 ? '0' + hour : hour;
-minute = minute <= 9 ? '0' + minute : minute;
-const zone = (now.getTimezoneOffset() / 60) * -1;
-const unixTime = (Date.now() / 1000) | 0;
-const timestamp = `<t:${unixTime}:t> - <t:${unixTime}:d>`;
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('out')
 		.setDescription('Selesai Bekerja'),
 	async execute(interaction) {
+		const now = new Date();
+		let hour = now.getHours();
+		let minute = now.getMinutes();
+		hour = hour <= 9 ? '0' + hour : hour;
+		minute = minute <= 9 ? '0' + minute : minute;
+		const zone = (now.getTimezoneOffset() / 60) * -1;
+		const unixTime = (Date.now() / 1000) | 0;
+		const timestamp = `<t:${unixTime}:t> - <t:${unixTime}:d>`;
+
 		const message = `[SIGN OUT]: ${interaction.user.tag} - ${hour}:${minute} [GMT]: ${zone}`;
 
 		const output = new EmbedBuilder()
