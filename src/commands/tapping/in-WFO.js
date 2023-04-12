6f58843f63bb1ded72b8472555f28eec1ffb3ce4
Immov <41504515+Immov@users.noreply.github.com>
2023-04-12 11:03:25 +0700

    fix time issue

--- src/commands/tapping/in-WFO.js
+++ src/commands/tapping/in-WFO.js
@@ -1,14 +1,15 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const tap = require('../../models/tap');
 const { insertDB } = require('../../functions/insertDB');
-const {
-	now,
-	hour,
-	minute,
-	zone,
-	unixTime,
-	timestamp,
-} = require('../../var/waktu');
+
+const now = new Date();
+let hour = now.getHours();
+let minute = now.getMinutes();
+hour = hour <= 9 ? '0' + hour : hour;
+minute = minute <= 9 ? '0' + minute : minute;
+const zone = (now.getTimezoneOffset() / 60) * -1;
+const unixTime = (Date.now() / 1000) | 0;
+const timestamp = `<t:${unixTime}:t> - <t:${unixTime}:d>`;
 
 module.exports = {
 	data: new SlashCommandBuilder()
--- src/commands/tapping/out.js
+++ src/commands/tapping/out.js
@@ -3,12 +3,9 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const now = new Date();
 let hour = now.getHours();
 let minute = now.getMinutes();
-// 2 Digits hour & minutes
 hour = hour <= 9 ? '0' + hour : hour;
 minute = minute <= 9 ? '0' + minute : minute;
-
 const zone = (now.getTimezoneOffset() / 60) * -1;
-
 const unixTime = (Date.now() / 1000) | 0;
 const timestamp = `<t:${unixTime}:t> - <t:${unixTime}:d>`;
 
